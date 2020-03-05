7de226b0987363cad718a205359e9e2c03e69b83
Ling Li <lingli@Lings-MacBook-Pro.local>
2020-03-05 09:27:29 -0500

    first commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_KEY = '68fbee842aac857a3d7723d5109b4f52';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_URL = 'http://image.tmdb.org/t/p/'
